2d97b9ee6cf5457ab17197c83e120b2133427611
Ilya Lonshakov <modnyi-l@mail.ru>
2026-03-18 11:08:29 +0700

    fix: fixed a problem with file names

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import rootReducer from './RootReducer';
+import rootReducer from '../services/RootReducer';
 
 import {
   TypedUseSelectorHook,
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,12 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /stories/, /\.stories\./, /\.test\./, /\.spec\./],
         use: ['babel-loader']
       },
       {
         test: /\.(ts)x?$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /stories/, /\.stories\./, /\.test\./, /\.spec\./],
         use: {
           loader: 'ts-loader'
         }
@@ -89,7 +89,6 @@ module.exports = {
     static: path.join(__dirname, './dist'),
     compress: true,
     historyApiFallback: true,
-    port: 4000,
-    open: true
+    port: 4000
   }
 };
